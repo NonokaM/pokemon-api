4cb43a1244a3f0a785065230a8d49ba35cfd640d
nonokaM <c1201071@gmail.com>
2023-06-06 23:52:30 +0900

    次ページへの機能

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -16,6 +17,7 @@ function App() {
 
       //各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
+      setNextURL(res.next);
       // console.log(res.results);
 
       // console.log(res);
@@ -35,7 +37,16 @@ function App() {
     setPokemonData(_pokemonData)
   };
 
-  console.log(pokemonData);
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setLoading(false);
+  };
+
+  const handlePrevPage = () => {};
 
   return (
     <>
@@ -49,6 +60,10 @@ function App() {
             {pokemonData.map((pokemon, i) => {
               return <Card key={i} pokemon={pokemon} />
             })}
+          </div>
+          <div className='btn'>
+            <button onClick={handlePrevPage}>Prev</button>
+            <button onClick={handleNextPage}>Next</button>
           </div>
           </>
         )}
